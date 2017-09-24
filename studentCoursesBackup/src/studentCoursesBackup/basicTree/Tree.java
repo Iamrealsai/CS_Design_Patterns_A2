162bc9d9639b09e73cc669a327ce0d7789d417b9
dinal herath <dinal.bing@gmail.com>
2017-09-24 19:50:18 -0400

    added constructor to tree

--- studentCoursesBackup/src/studentCoursesBackup/basicTree/Tree.java
+++ studentCoursesBackup/src/studentCoursesBackup/basicTree/Tree.java
@@ -4,5 +4,24 @@ import studentCoursesBackup.myTree;
 
 class Tree
 {
+    private Node root; // the only data field in the tree
 
+    public Tree(){
+	root = null;
+    }
+    
+    /*    public Node find(int key){
+	Node current = root;
+	while(current.getBNumber()!=key){
+	    if(key<current.getBNumber()){
+		current = current.getLeftChild();
+	    }else{
+		current = current.getRightChild();
+	    }
+	    if(current==null){
+		return null;
+	    }
+	}
+	return current;
+    }*/
 }
