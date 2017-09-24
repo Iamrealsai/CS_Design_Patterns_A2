0d64af33be4e5b5cfe76d5438648c3e8db93de90
dinal herath <dinal.bing@gmail.com>
2017-09-24 19:42:01 -0400

    added documentation for Node.java

--- studentCoursesBackup/src/studentCoursesBackup/myTree/Node.java
+++ studentCoursesBackup/src/studentCoursesBackup/myTree/Node.java
@@ -10,6 +10,10 @@ class Node
     private Node leftChild;
     private Node rightChild;
 
+    /**
+     *Constructor
+     *@param the BNumber of student
+     **/
     public Node(int bNumberIn){
 	bNumber = bNumberIn;
 	subjects = new ArrayList<String>();
@@ -18,6 +22,10 @@ class Node
 	rightChild = null;
     }
 
+    /**
+     *gets BNumber of student
+     *@return the BNumber
+     **/
     public int getBNumber(){
 	return bNumber;
     }
@@ -26,18 +34,34 @@ class Node
       bNumber = xIn;
       }*/
 
+    /**
+     *gets the left child of node
+     *@return the left child node
+     **/
     public Node getLeftChild(){
 	return leftChild;
     }
 
+    /**
+     *sets the left child for node
+     *@param the left child
+     **/
     public void setLeftChild(Node nodeIn){
 	leftChild = nodeIn;
     }
 
+    /**
+     *gets the right child of node
+     *@return the right child
+     **/
     public Node getRightChild(){
 	return rightChild;
     }
 
+    /**
+     *inserts the subject for each student
+     *@param s the String name of the subject
+     **/
     public void insertSubject(String s){
 	if(!subjects.contains(s)){
 	    subjects.add(s);
