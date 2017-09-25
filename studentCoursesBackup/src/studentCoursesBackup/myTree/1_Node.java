7cac7e2075046d362b593a182da036238ce0cc21
dinal herath <dinal.bing@gmail.com>
2017-09-25 08:58:11 -0400

    created constructor

--- studentCoursesBackup/src/studentCoursesBackup/myTree/1_Node.java
+++ studentCoursesBackup/src/studentCoursesBackup/myTree/1_Node.java
@@ -7,5 +7,24 @@ import studentCoursesBackup.myTree.ObserverInterface;
 
 class Node implements BSTNodeInterface, SubjectInterface, ObserverInterface
 {
+    private int bNumber; //unique ID
+    private ArrayList<String> subjects;//subject list
+    private ArrayList<BSTNodeInterface> observers;//for observers
+    private BSTNodeInterface leftChild; // left child
+    private BSTNodeInterface rightChild;//right child
 
+    /**
+     *Constructor
+     *@param bNumberin, the BNumber of student
+     **/
+    public Node(int bNumberIn){
+	bNumber = bNumberIn;
+	subjects = new ArrayList<String>();
+	observers = new ArrayList<BSTNodeInterface>();
+	leftChild = null;
+	rightChild = null;
+    }
+
+    
+    
 }
