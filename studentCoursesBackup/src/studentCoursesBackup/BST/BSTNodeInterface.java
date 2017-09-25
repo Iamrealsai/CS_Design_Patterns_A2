89a2f6c4d613a9a2d6e653d6dbba974972ff0b86
dinal herath <dinal.bing@gmail.com>
2017-09-25 08:48:19 -0400

    created ObserverInterface.java with update()method

--- studentCoursesBackup/src/studentCoursesBackup/BST/BSTNodeInterface.java
+++ studentCoursesBackup/src/studentCoursesBackup/BST/BSTNodeInterface.java
@@ -8,4 +8,6 @@ public interface BSTNodeInterface
     public void setLeftChild(BSTNodeInterface nodeIn);
     public int getLastOperation();//temporary method-> to see if node deleted Or inserted in last occuource
     public int setLastOperation(int iIn);//temporary method -> sets the last operation 1-for insertion, 2-for deletion
+    public void insertSubject(String sIn);//to insert a subject
+    public void deleteSubject(String sIn);//to delete a subject
 }
---
+++ studentCoursesBackup/src/studentCoursesBackup/myTree/ObserverInterface.java
@@ -0,0 +1,6 @@
+package studentCoursesBackup.myTree;
+
+public interface ObserverInterface
+{
+    public void update(int OperationIndex,String sIn)//operationIndex-1 for insert,2 for delete, sIn for the subjectstring;
+}
