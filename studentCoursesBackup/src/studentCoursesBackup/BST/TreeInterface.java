765426c9c37eae688d7616c7b75783dc103dfe24
dinal herath <dinal.bing@gmail.com>
2017-09-25 11:42:30 -0400

    commented out getRootNode() from TreeInterface bacuse there are three roots

--- studentCoursesBackup/src/studentCoursesBackup/BST/TreeInterface.java
+++ studentCoursesBackup/src/studentCoursesBackup/BST/TreeInterface.java
@@ -3,7 +3,7 @@ package studentCoursesBackup.BST;
 public interface TreeInterface
 {
     public BSTNodeInterface find(BSTNodeInterface root,int index);
-    public BSTNodeInterface getRootNode();
+    // public BSTNodeInterface getRootNode();
     public void insertNode(BSTNodeInterface nodeIn);
     public String printInorder();
 }
