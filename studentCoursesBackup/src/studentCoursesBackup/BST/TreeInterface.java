116ed508f64be551c4f8c7b3f9e3a45a7abd24f3
dinal herath <dinal.bing@gmail.com>
2017-09-25 11:41:54 -0400

    updated TreeInterface.java and implementing-> find()

---
+++ studentCoursesBackup/src/studentCoursesBackup/BST/TreeInterface.java
@@ -0,0 +1,9 @@
+package studentCoursesBackup.BST;
+
+public interface TreeInterface
+{
+    public BSTNodeInterface find(BSTNodeInterface root,int index);
+    public BSTNodeInterface getRootNode();
+    public void insertNode(BSTNodeInterface nodeIn);
+    public String printInorder();
+}
--- studentCoursesBackup/src/studentCoursesBackup/util/TreeBuilder.java
+++ studentCoursesBackup/src/studentCoursesBackup/util/TreeBuilder.java
@@ -1,8 +1,20 @@
 package studentCoursesBackup.util;
 
-import studentCoursesBackup.myTree;
+import studentCoursesBackup.BST;
+import studentCoursesBackup.myTree.Node;
 
-class TreeBuilder
+class TreeBuilder implements TreeInterface
 {
+    private BSTNodeInterface masterRoot;//root node for main tree
+    private BSTNodeInterface backupRoot1;//root node for backup 1 tree
+    private BSTNodeInterface backupRoot2;//root node for backup 2 tree
 
+    //----------------TreeInterface--------------------
+
+    public BSTNodeInterface find(BSTNodeInterface root,int index){
+	BSTNodeInterface current = root;
+	
+    }
+    
+    //-------------------------------------------------
 }
