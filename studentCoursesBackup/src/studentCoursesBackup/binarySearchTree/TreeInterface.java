c90d88e1b100faa413b250df4be3a7ba72fab7c1
dinal herath <dinal.bing@gmail.com>
2017-09-26 20:42:26 -0400

    configured TreeInterface.java

--- studentCoursesBackup/src/studentCoursesBackup/binarySearchTree/TreeInterface.java
+++ studentCoursesBackup/src/studentCoursesBackup/binarySearchTree/TreeInterface.java
@@ -2,8 +2,8 @@ package studentCoursesBackup.binarySearchTree;
 
 public interface TreeInterface
 {
-    public BSTNodeInterface find(BSTNodeInterface root,int index);
+    public Node find(Node root,int index);
     // public BSTNodeInterface getRootNode();
-    public void insertNode(BSTNodeInterface root,int index);
-    public String printNodes(BSTNodeInterface root);
+    public void insertNode(Node root,int index);
+    public String printNodes(Node root);
 }
