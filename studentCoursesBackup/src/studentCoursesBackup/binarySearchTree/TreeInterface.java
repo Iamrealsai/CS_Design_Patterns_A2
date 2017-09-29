eacf0313f6befe28e924a95eee62c1e81320d15c
dinal herath <dinal.bing@gmail.com>
2017-09-29 12:08:57 -0400

    canged TreeINterface.java

--- studentCoursesBackup/src/studentCoursesBackup/binarySearchTree/TreeInterface.java
+++ studentCoursesBackup/src/studentCoursesBackup/binarySearchTree/TreeInterface.java
@@ -4,7 +4,7 @@ import studentCoursesBackup.myTree.Node;
 
 public interface TreeInterface
 {
-    Node find(Node root,int index);
+    Node find(int index);
     // public BSTNodeInterface getRootNode();
     void insertNode(Node root,int index);
     String printNodes(Node root);
