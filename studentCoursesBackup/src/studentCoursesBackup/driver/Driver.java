85be825d50d30c49d767c58dd59bf1962d76656f
dinal herath <dinal.bing@gmail.com>
2017-09-30 12:40:50 -0400

    done formatting of driver file

--- studentCoursesBackup/src/studentCoursesBackup/driver/Driver.java
+++ studentCoursesBackup/src/studentCoursesBackup/driver/Driver.java
@@ -11,7 +11,7 @@ public class Driver
 
 	//Name - Jerome Dinal Herath Muthukumaranage
 	//B no - B00708543
-	//check for no of arugments
+	//email - jherath1@binghamton.edu
 	try{
 	    for(int i=0;i<args.length;i++){
 		if(args[i].equals("${arg"+i+"}") || args.length!=5){
